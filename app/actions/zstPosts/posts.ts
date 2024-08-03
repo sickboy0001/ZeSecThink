58cd7b6227173720e4edfac0cb3a243be8292814
tfujiki <syunjyu0001@gmail.com>
2024-08-03 13:33:35 +0900

    Query調整

--- app/actions/zstPosts/posts.ts
+++ app/actions/zstPosts/posts.ts
@@ -26,9 +26,9 @@ export const getPosts = async (
     .select("*")
     .eq("user_id", user_id)
     // .gte("current_at", getJpTimeZoneFromUtc(from_at))
-    .gte("current_at", getJpTimeZoneFromUtc(from_at))
+    .gte("current_at", GetDateTimeFormat(from_at))
     // .lte("current_at", getJpTimeZoneFromUtc(to_at))
-    .lte("current_at", getJpTimeZoneFromUtc(to_at))
+    .lte("current_at", GetDateTimeFormat(to_at))
     .order("current_at", { ascending: false }) // 最初のソート条件: true 昇順
     .order("write_start_at", { ascending: false }) // 次のソート条件: false 降順;
     .order("update_at", { ascending: false }); // 次のソート条件: 降順;
