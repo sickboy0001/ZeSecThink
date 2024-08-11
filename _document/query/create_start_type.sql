b65e90f27817ab1144aac1689be5e36bc9b288f0
tfujiki <syunjyu0001@gmail.com>
2024-08-12 05:21:12 +0900

    Update .gitignore to ignore _document folder

--- _document/query/create_start_type.sql
+++
@@ -1,9 +0,0 @@
-create table
-  public.start_type (
-    id serial,
-    disp_name text not null,
-    title_name text not null,
-    name text not null,
-    display_order integer not null,
-    constraint start_type_pkey primary key (id)
-  ) tablespace pg_default;
--- _document/trach/day_page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ViewDay = () => {
-  return <div></div>;
-};
-
-export default ViewDay;
